13028fe569be995fb6cdf24032dfe065c84d3803
Tom Guy <tom.guy@campus.technion.ac.il>
2021-06-06 13:16:01 +0300

    Initial commit

---
+++ SortedList.h
@@ -0,0 +1,22 @@
+//
+// Created by tomgu on 06/06/2021.
+//
+
+#ifndef EX2_SORTEDLIST_H
+#define EX2_SORTEDLIST_H
+
+
+class SortedList {
+
+public:
+    void push_back(int data);
+
+    void clear();
+
+private:
+
+    void MyUtility();
+};
+
+
+#endif //EX2_SORTEDLIST_H
---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by tomgu on 06/06/2021.
+//
+
+#include "SortedList.h"
+
+int main()
+{
+    SortedList ls;
+    ls.push_back(1);
+    ls.clear();
+}
