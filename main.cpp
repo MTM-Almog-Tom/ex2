a82b161f11776117395b848c5f82453773147fd7
Tom-Guy84 <83006932+Tom-Guy84@users.noreply.github.com>
2021-06-08 15:20:41 +0300

    update sorted list

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
 //
-// Created by tomgu on 06/06/2021.
+// Created by tomgu on 08/06/2021.
 //
 
-#include "SortedList.h"
+#include <iostream>
+#include "sortedList.h"
+#define TEST(num) cout << endl << "TEST " << (num) << endl;
+
+using std::cout;
+using std::endl;
+using std::string;
+using namespace mtm;
+
+template<class T>
+void printList(SortedList<T> list);
 
 int main()
 {
-    SortedList ls;
-    ls.push_back(1);
-    ls.clear();
+    TEST("1.5")
+    SortedList<string> lst1 = SortedList<string>();
+    lst1.insert("Charlie");
+    lst1.insert("Bob");
+    lst1.insert("Alice");
+    lst1.insert("Donald");
+
+    printList(lst1);
+
+}
+
+template<class T>
+void printList(SortedList<T> list) {
+    for (auto it = list.begin(); !(it == list.end()); ++it) {
+        cout << *it << endl;
+    }
+    cout << endl;
 }
