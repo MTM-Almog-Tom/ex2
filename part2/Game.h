4cba3fa0c724470ec2be09e9096b7aa91e447df6
Tom-Guy84 <83006932+Tom-Guy84@users.noreply.github.com>
2021-06-15 07:58:45 +0300

    add game to operator <<

--- part2/Game.h
+++ part2/Game.h
@@ -36,7 +36,7 @@ namespace mtm
         std::ostream &printGameBoard(std::ostream &os, const char *begin,
                                      const char *end, unsigned int width) const;
         bool isOver(Team *winningTeam = NULL) const;
-        std::ostream &operator<<(std::ostream &os);                                         // operator <<
+        std::ostream &operator<<(std::ostream &os, Gam& game);                                   // operator <<
 
         ~Game();
     };
