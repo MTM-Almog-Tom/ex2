ad41ce3a1c5bf3b08ee7ccc8ba685eae7bd41696
Tom-Guy84 <83006932+Tom-Guy84@users.noreply.github.com>
2021-06-16 19:36:56 +0300

    update examDetails according to demands.

--- examDetails.cpp
+++ examDetails.cpp
@@ -1,19 +1,28 @@
 #include "examDetails.h"
+#include <cmath>
 
-// constractor of ExamDetails
-ExamDetails::ExamDetails(int course_number, int month, int day, double starting_time, int duration_time, string zoom_link)
+const double epsilon = pow(10,-6);
+const int max_month = 12;
+const int min_month = 0;
+const int max_day = 30;
+const int min_day = 0;
+const double half = 0.5;
+const int min_duration_time = 0;
+
+// constructor of ExamDetails
+ExamDetails::ExamDetails(int course_number, int month, int day, double starting_time, int duration_time, std::string zoom_link)
 {
-    if (course_number < 100000 || course_number > 999999)
-    {
-        throw InvalidArgsException();
-    }
-    if (month < 0 || month > 12 || day < 0 || day > 30)
+    if (month < min_month || month > max_month || day < min_day || day > max_day)
     {
         throw InvalidDateException();
     }
-    if ((starting_time - (int)starting_time != 0.5) && (starting_time - (int)starting_time != 0) || duration_time < 0)
+    if ((std::abs(ceil(starting_time) - starting_time - half) < epsilon) && (std::abs(starting_time - ceil(starting_time)) < epsilon))
     {
         throw InvalidTimeException();
+    }
+    if(duration_time < min_duration_time)
+    {
+        throw InvalidArgsException();
     }
     this->course_number = course_number;
     this->month = month;
@@ -23,7 +32,7 @@ ExamDetails::ExamDetails(int course_number, int month, int day, double starting_
     this->zoom_link = zoom_link;
 }
 
-// copy constractor if ExamDetails
+// copy constructor if ExamDetails
 ExamDetails::ExamDetails(const ExamDetails &copy_from)
 {
     this->course_number = copy_from.course_number;
@@ -34,22 +43,11 @@ ExamDetails::ExamDetails(const ExamDetails &copy_from)
     this->zoom_link = copy_from.zoom_link;
 }
 
-//copy assignment for assigning an ExamDetails from one ExamDetails to another
-ExamDetails &ExamDetails::operator=(const ExamDetails &copy_from)
-{
-    this->course_number = copy_from.course_number;
-    this->month = copy_from.month;
-    this->day = copy_from.day;
-    this->starting_time = copy_from.starting_time;
-    this->duration_time = copy_from.duration_time;
-    this->zoom_link = copy_from.zoom_link;
-    return *this;
-}
-string ExamDetails::getLink() const
+std::string ExamDetails::getLink() const
 {
     return this->zoom_link;
 }
-void ExamDetails::setLink(string zoom_link)
+void ExamDetails::setLink(std::string zoom_link)
 {
     this->zoom_link = zoom_link;
 }
@@ -74,22 +72,21 @@ ExamDetails ExamDetails::makeMatamExam()
 
 void ExamDetails::print() const
 {
-    cout << *this;
+    std::cout << *this;
 }
 std::ostream &operator<<(std::ostream &os, const ExamDetails &r)
 {
-    os << "Course Number: " << r.course_number << endl
+    os << "Course Number: " << r.course_number << std::endl
        << "Time: " << r.day << "." << r.month << " at " << (int)r.starting_time;
     if (r.starting_time - (int)r.starting_time == 0)
     {
-        os << ":00" << endl;
+        os << ":00" << std::endl;
     }
     else
     {
-        os << ":30" << endl;
+        os << ":30" << std::endl;
     }
-    os << "Duration: " << r.duration_time << ":00" << endl
+    os << "Duration: " << r.duration_time << ":00" << std::endl
        << "zoom link: " << r.zoom_link;
     return os;
 }
-ExamDetails::~ExamDetails() {}
