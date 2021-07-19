9de9029ac8151b501df50912516c9341fc43a03a
Tom-Guy84 <83006932+Tom-Guy84@users.noreply.github.com>
2021-07-19 13:22:59 +0300

    Fixed part A
    Add const in SortedList in length, apply and filter.
    Add namspace mtm in Exam.
    Passes valgrind.

---
+++ partA/cmake-build-debug/CMakeFiles/313151185_315155671.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/tmp/tmp.wlBQUSSP1w/examDetails.cpp" "/tmp/tmp.wlBQUSSP1w/cmake-build-debug/CMakeFiles/313151185_315155671.dir/examDetails.cpp.o"
+  "/tmp/tmp.wlBQUSSP1w/part_a_test.cpp" "/tmp/tmp.wlBQUSSP1w/cmake-build-debug/CMakeFiles/313151185_315155671.dir/part_a_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../partA"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ partA/cmake-build-debug/CMakeFiles/313151185_315155671.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "313151185_315155671"
+  "313151185_315155671.pdb"
+  "CMakeFiles/313151185_315155671.dir/examDetails.cpp.o"
+  "CMakeFiles/313151185_315155671.dir/part_a_test.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/313151185_315155671.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
